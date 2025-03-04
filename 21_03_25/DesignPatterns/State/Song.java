a6fabc2e395f553cc1a0b5a376ce3b1a3e05b003
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-03-04 22:59:38 +0100

    Add parametrized comments

--- 21_03_25/DesignPatterns/State/Song.java
+++ 21_03_25/DesignPatterns/State/Song.java
@@ -3,6 +3,11 @@ public class Song {
     private String author;
     private int secondsDuration;
     
+    /** Constructor needs the title, the author, and the duration in seconds. 
+     * @param t : The title of the song
+     * @param au : The author of the song
+     * @param durat : The duration of the song, in seconds
+    */
     public Song(String t, String au, int durat){
         this.title = t;
         this.author = au;
@@ -18,6 +23,9 @@ public class Song {
     public int getSecondsDuration() {
         return secondsDuration;
     }
+
+    /**Override the toString method in order to print song details.*/ 
+    @Override
     public String toString(){
         return this.title + " by " + this.author + " - " + (this.secondsDuration)/60 + "m";
     }
