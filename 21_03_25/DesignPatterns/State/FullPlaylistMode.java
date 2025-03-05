516f2dfee1f363c4ac66e61f36480d2d0a96376c
fed <fed@localhost.localdomain>
2025-03-05 11:13:08 +0100

    Add UML diagram to MusicPlayer class

--- 21_03_25/DesignPatterns/State/FullPlaylistMode.java
+++ 21_03_25/DesignPatterns/State/FullPlaylistMode.java
@@ -1,4 +1,5 @@
 //ConcreteStateA
+
 public class FullPlaylistMode implements MyDisplay{
        private SongLibrary songlib;
        private String modeTitle = "FullPlaylist";
@@ -10,18 +11,23 @@ public class FullPlaylistMode implements MyDisplay{
 
        public void visualizza(Song s){
               System.out.println("Printing library...");
+              printAllList();
+              System.out.println(songlib.getWhereInList()+1 + " ------ " +s);
+              System.out.println("-----\n");
+       }
+
+       public String getModeTitle() {
+           return modeTitle;
+       }
+
+       private void printAllList(){
               int i = 1;
               System.out.println("-----");
-              for(Song song : songlib.getMyLibrary()){
+              for (Song song : songlib.getMyLibrary()){
                      System.out.println(i + " -- " + song);
                      i++;
               }
               System.out.println("-----");
-              System.out.println(songlib.getWhereInList()+1 + " ------ " +s);
-              System.out.println("-----\n");
 
-       }
-       public String getModeTitle() {
-           return modeTitle;
        }
 }
